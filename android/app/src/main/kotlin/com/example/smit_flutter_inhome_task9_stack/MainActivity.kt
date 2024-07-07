47c7823577673fbf6dcdce7b57d22856126a72f1
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-07-07 05:26:06 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/smit_flutter_inhome_task9_stack/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smit_flutter_inhome_task9_stack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceheight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/background.jpg',
+              height: deviceheight * 0.3,
+              fit: BoxFit.fill,
+            ),
+            Positioned(
+              child: Container(
+                height: deviceheight * .25,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10), color: Colors.red),
+              ),
+            ),
+            Positioned(
+              child: Container(
+                height: deviceheight * .1,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
